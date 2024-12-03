b6fa8c589ce6992e290241344d16eff5597c7a3c
Elisa <33760@student.mboutrecht.nl>
2024-12-03 10:05:39 +0100

    playerMovementWip

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -25,8 +25,8 @@ public class PlayerCam : MonoBehaviour
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
-        xRotation -= mouseY;  // Invert mouseY here to fix the inversion issue.
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // Fix the range of xRotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
